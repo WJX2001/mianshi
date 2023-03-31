fcff87fa21f73dc521e1ca07daa4de5e6dcae111
WJX2001 <1758807220@qq.com>
2023-03-31 19:09:37 +0800

    发布订阅者模式

---
+++ js/观察者和发布者订阅者模式/2.发布订阅者模式.js
@@ -0,0 +1,34 @@
+// on 是订阅 emit 发布
+
+// 邮局
+let e = {
+    // 存订阅者
+    _callback: [],
+    on(callback){
+        this._callback.push(callback)
+    },
+    // 发布者
+    emit(value) {
+        this._callback.forEach(method => {
+            method(value)
+        })
+    }
+}
+// 订阅
+e.on(function(value){
+    console.log('张三订阅：'+value)
+})
+// 订阅
+e.on(function(value){
+    console.log('李四订阅：'+value)
+})
+// 订阅
+e.on(function(value){
+    console.log('王五订阅：'+value)
+})
+
+
+// 发布
+e.emit('中央日报')
+e.emit('镇江日报')
+
