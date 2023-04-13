104cd57e94d528b887f16479783ad9247715d403
WJX2001 <1758807220@qq.com>
2023-04-13 20:46:51 +0800

    判定是否互为字符重排

---
+++ algorithm/字符串处理专项/2.判定是否互为字符重排.js
@@ -0,0 +1,22 @@
+var CheckPermutation = function(s1, s2) {
+    var CheckPermutation = function(s1, s2) {
+        if(s1.length !== s2.length) return false
+        let map = new Map()
+    
+        for(let v of s1) {
+           map.set(v,(map.get(v) ||0)+1)
+        }
+    
+        for(let v2 of s2) {
+    
+            if(!map.has(v2) || map.get(v2) <=0){
+                return false
+            }
+            if(map.has(v2)){
+                map.set(v2,map.get(v2)-1)
+            }
+        }
+        
+        return true
+    };
+}
