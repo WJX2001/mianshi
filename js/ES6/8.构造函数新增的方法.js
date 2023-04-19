fc15d078bab70fb6707ce600b5010a11109b5128
WJX2001 <1758807220@qq.com>
2023-04-19 19:37:20 +0800

    ES6 常见操作

--- js/ES6/8.构造函数新增的方法.js
+++ js/ES6/8.构造函数新增的方法.js
@@ -1,11 +1,28 @@
 
 // Array.from  和 Array.of()
 // TODO: Array.from()
-let arrayLike = {
-    '0':'a',
-    '1':'b',
-    '2':'c',
-    length:3
-}
-let arr2 = Array.from(arrayLike)
-console.log(arr2)
+// let arrayLike = {
+//     '0':'a',
+//     '1':'b',
+//     '2':'c',
+//     length:3
+// }
+// let arr2 = Array.from(arrayLike)
+// console.log(arr2)
+
+
+// TODO: copyWithin
+/**
+ * 将指定位置的成员复制到其他位置（会覆盖原有成员），然后返回当前数组
+ */
+
+let arr = [1,2,3,4,5]
+console.log(arr.copyWithin(0,3))
+
+
+
+
+
+
+
+
---
+++ js/ES6/9.entries,keys,values.js
@@ -0,0 +1,18 @@
+let obj = {
+    name: 'wjx',
+    age: 16,
+    sex: 'man'
+}
+
+console.log(Object.keys(obj))
+
+// TODO: entries 的用法
+
+const obj1 = {
+    name: 'John',
+    age: 25,
+    city: 'New York'
+};
+const obj2 = Object.entries(obj1)
+console.log(obj2)
+  
